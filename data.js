6443587bee147fa029323f852e02a6e69d6c2aa9
MANSUN KASYAP <78608125+mansunkasyap@users.noreply.github.com>
2023-09-30 23:07:35 +0530

    Create data.js
    Here Data is Stored in Object format

---
+++ data.js
@@ -0,0 +1,57 @@
+const data = [
+    {
+        id: 1,
+        title: "The First One",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Obcaecati nesciunt accusamus molestiae totam laboriosam ",
+        img: "card1.jpg",
+        price: 120,
+        stats: {
+            rating: 5.0,
+            reviewCount: 20
+        },
+        location: "Online",
+        openSpots: 26
+    },
+
+    {
+        id: 2,
+        title: "The Second Thing",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Obcaecati nesciunt accusamus molestiae totam laboriosam ",
+        img: "card2.jpg",
+        price: 130,
+        stats: {
+            rating: 5.0,
+            reviewCount: 22
+        },
+        location: "Online",
+        openSpots: 25
+    },
+
+    {
+        id: 3,
+        title: "The Third Option",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Obcaecati nesciunt accusamus molestiae totam laboriosam",
+        img: "card3.jfif",
+        price: 120,
+        stats: {
+            rating: 4.7,
+            reviewCount: 20
+        },
+        location: "Online",
+        openSpots: 34
+    },
+    {
+        id: 4,
+        title: "The Fourth Option",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Obcaecati nesciunt accusamus molestiae totam laboriosam",
+        img: "card1.jpg",
+        price: 150,
+        stats: {
+            rating: 4.8,
+            reviewCount: 20
+        },
+        location: "Online",
+        openSpots: 34
+    }
+]
+ export default data;
